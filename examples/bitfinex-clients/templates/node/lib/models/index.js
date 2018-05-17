682ce435b7de381e1ac45b58189a3d640d636d93
hems <hems.inlet@gmail.com>
2018-05-17 04:41:21 +0100

    template for node/lib/model/index.js

--- examples/bitfinex-clients/templates/node/lib/models/index.js
+++ examples/bitfinex-clients/templates/node/lib/models/index.js
@@ -1,24 +1,9 @@
 'use strict'
 
 const models = {
-  OrderBook: require('./order_book'),
-  BalanceInfo: require('./balance_info'),
-  FundingCredit: require('./funding_credit'),
-  FundingInfo: require('./funding_info'),
-  FundingLoan: require('./funding_loan'),
-  FundingOffer: require('./funding_offer'),
-  FundingTrade: require('./funding_trade'),
-  MarginInfo: require('./margin_info'),
-  Notification: require('./notification'),
-  Order: require('./order'),
-  Position: require('./position'),
-  Trade: require('./trade'),
-  Wallet: require('./wallet'),
-  Alert: require('./alert'),
-  TradingTicker: require('./trading_ticker'),
-  FundingTicker: require('./funding_ticker'),
-  PublicTrade: require('./public_trade'),
-  Candle: require('./candle')
+  {{#each node.models}}
+  {{pascal_case @key}}: require('./{{@key}}'){{#unless @last}},{{/unless}}
+  {{/each}}
 }
 
 module.exports = models
